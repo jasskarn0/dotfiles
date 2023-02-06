0ce7349c0c471088df64602aba28e990e6e6fe08
jasskarn0 <1998.jksandhu@gmail.com>
2023-02-06 02:17:19 -0800

    startup-script

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 !/bin/bash
 
-sudo pacman -Syyu i3-wm vim neofetch thunar alacritty picom firefox git base-devel sddm
+sudo pacman -Syyu i3-wm vim neofetch thunar alacritty picom firefox git base-devel sddm arandr
 
 git clone https://aur.archlinux.org/yay.git
 

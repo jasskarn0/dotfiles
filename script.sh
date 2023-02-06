45f303d5cb5efb79f4b5ce2e6561c0f76ca77d50
jasskarn0 <1998.jksandhu@gmail.com>
2023-02-06 02:14:59 -0800

    startup-script

---
+++ script.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+
+sudo pacman -Syyu i3-wm vim neofetch thunar alacritty picom firefox git base-devel sddm
+
+git clone https://aur.archlinux.org/yay.git
+
+cd yay
+
+makepkg -si
+
+sudo systemctl enable sddm
+
+sudo systemctl set-default graphical.target

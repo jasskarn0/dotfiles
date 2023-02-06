7001f45e4245d877ea0494d87e535b0c41e7775c
jasskarn0 <1998.jksandhu@gmail.com>
2023-02-06 14:12:28 -0800

    startup-script

--- script.sh
+++ script.sh
@@ -1,12 +1,14 @@
 !/bin/bash
 
-sudo pacman -Syyu i3-wm vim neofetch thunar alacritty picom firefox git base-devel sddm arandr
+sudo pacman -Syyu i3-wm vim neofetch thunar alacritty picom firefox git base-devel sddm arandr xorg-server
 
 git clone https://aur.archlinux.org/yay.git
 
 cd yay
 
 makepkg -si
+
+yay -S ly
 
 sudo systemctl enable sddm
 

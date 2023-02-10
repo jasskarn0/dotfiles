f4cdbeff9b0b58294d32582ff960d483856edf13
jasskarn0 <1998.jksandhu@gmail.com>
2023-02-10 04:49:22 +0000

    New files

--- script.sh
+++ script.sh
@@ -10,6 +10,8 @@ cp sxhkd ~/.config/sxhkdrc/
 
 mkdir ~/.powerlevel10k
 
+sudo cp "MesloLGS NF Regular.ttf" /usr/share/fonts
+
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
 echo 'source ~/.powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 

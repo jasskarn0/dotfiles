3ebc5be28ec6d6da7437b5be1b48f5e5e0433485
jasskarn0 <1998.jksandhu@gmail.com>
2023-02-10 05:00:01 +0000

    new files

--- script.sh
+++ script.sh
@@ -1,16 +1,21 @@
-!/bin/bash
+#!/bin/bash
 
 sudo pacman -Syyu sxhkd bspwm vim neofetch thunar alacritty picom firefox base-devel arandr xorg-server polybar zsh
 
 mkdir ~/.config/bspwm
 mkdir ~/.config/sxhkd
+mkdir ~/.config/alacritty
 
 cp bspwmrc ~/.config/bspwmrc/
 cp sxhkd ~/.config/sxhkdrc/
+cp picom.conf ~/.config/
+cp alacritty.yml ~/.config/alacritty/alacritty.yml
 
 mkdir ~/.powerlevel10k
 
 sudo cp "MesloLGS NF Regular.ttf" /usr/share/fonts
+
+
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
 echo 'source ~/.powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc

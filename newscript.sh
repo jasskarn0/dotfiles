90c934513411c2fc08ebf7120377c9c2149cf39a
jasskarn0 <1998.jksandhu@gmail.com>
2023-02-11 23:10:56 -0800

    updated

---
+++ newscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir ~/.config/bspwm
+mkdir ~/.config/sxhkd
+mkdir ~/.config/alacritty
+
+cp bspwmrc ~/.config/bspwm/
+cp sxhkdrc ~/.config/sxhkd/
+cp picom.conf ~/.config/
+
+mkdir ~/.powerlevel10k

e672ac84fa2b5d9400017c9d1ad80bd44e9a9d76
jasskarn0 <1998.jksandhu@gmail.com>
2023-02-16 16:20:05 -0800

    updated

--- script.sh
+++ script.sh
@@ -15,6 +15,9 @@ xrdb -merge ~/.Xresources
 #mkdir ~/.powerlevel10k
 #!/bin/bash
 
+############################################
+MAKE THE DIRECTORIES FOR THE CONFIG FILES
+############################################
 # Define an array of directory paths
 DIR_PATHS=(~/.config ~/.config/bspwm ~/.config/sxhkd ~/.config/alacritty ~/.powerlevel10k)
 
@@ -30,15 +33,43 @@ do
     echo "Directory $DIR_PATH created"
   fi
 done
+###########################################
+COPY FILES INTO DIRECTORIES MADE
+###########################################
+# Define the source directory containing the files to copy
+SOURCE_DIR="."
 
+# Define the destination directories to copy the files to
+DESTINATION_DIRS=(
+  "$HOME/.config/bspwm"
+  "$HOME/.config/sxhkd"
+  "$HOME/.config"
+  "$HOME/.config/alacritty"
+  "$HOME"
+)
 
-#Copying dotfiles
-cp bspwmrc ~/.config/bspwm/
-cp sxhkdrc ~/.config/sxhkd/
-cp picom.conf ~/.config/
-cp alacritty.yml ~/.config/alacritty/alacritty.yml
-rm ~/.zshrc
-cp .zshrc ~/.zshrc
+# Define an array of file names to copy
+FILE_NAMES=("bspwmrc" "sxhkdrc" "picom.conf" "alacritty.yml" ".zshrc")
+
+# Loop through the array of file names and copy each file to the corresponding destination directory
+for i in "${!FILE_NAMES[@]}"
+do
+  FILE_NAME="${FILE_NAMES[$i]}"
+  DESTINATION_DIR="${DESTINATION_DIRS[$i]}"
+
+  # Check if the file already exists in the destination directory
+  if [ -e "${DESTINATION_DIR}/${FILE_NAME}" ]
+  then
+    # If the file already exists, delete it
+    rm "${DESTINATION_DIR}/${FILE_NAME}"
+    echo "Deleted existing file ${FILE_NAME} in ${DESTINATION_DIR}"
+  fi
+
+  # Copy the file to the destination directory
+  cp "${SOURCE_DIR}/${FILE_NAME}" "${DESTINATION_DIR}"
+  echo "Copied file ${FILE_NAME} to ${DESTINATION_DIR}"
+done
+############################################
 
 #cursor set
 sudo cp -R volantes_cursors /usr/share/icons/

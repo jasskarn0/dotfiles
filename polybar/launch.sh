f83cc9a5da53ae33442abd96c5c1cae2edb348ed
jasskarn0 <1998.jksandhu@gmail.com>
2024-01-17 15:50:44 -0800

    done

--- polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-killall -q polybar
-
-polybar -c ~/.config/polybar/left.ini mybar & disown
-polybar -c ~/.config/polybar/center.ini center & disown
-polybar -c ~/.config/polybar/right.ini right & disown
-
-echo "done done done"
